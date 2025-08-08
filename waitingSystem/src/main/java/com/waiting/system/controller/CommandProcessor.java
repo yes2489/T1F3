e0dc4cda619e2127f9aa9546a226259887403ecd
yes2489 <yes24150@gmail.com>
2025-08-08 14:59:12 +0900

    chore: CommandProcessor 클래스 내 processCommand 추가

--- waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
+++ waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
@@ -1,4 +1,43 @@
 package com.waiting.system.controller;
 
+import com.waiting.system.util.UIHandler;
+
 public class CommandProcessor {
+    /**
+     * 전체 명령 처리 진입점
+     * @param input 사용자 입력 문자열
+     */
+    public void processCommand(String input) {
+        String[] tokens = UIHandler.getCommand(input);
+        if (tokens.length == 0) return;
+
+        String command = tokens[0].toLowerCase();
+
+        switch (command) {
+            case "reservation":
+                handleReservation(tokens);
+                break;
+            case "cancel":
+                handleCancel(tokens);
+                break;
+            case "quit":
+                handleQuit(tokens);
+                break;
+            default:
+                UIHandler.addLine("❌ 지원하지 않는 명령어입니다.");
+        }
+        UIHandler.flush(""); // 결과 출력
+    }
+
+    private void handleQuit(String[] tokens) {
+    }
+
+    private void handleCancel(String[] tokens) {
+    }
+
+    private void handleReservation(String[] tokens) {
+    }
+
+
+
 }
