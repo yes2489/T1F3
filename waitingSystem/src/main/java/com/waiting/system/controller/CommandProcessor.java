e5d30e1622d0e8ad12da1e477ff0f440ed8ecb28
TaekkiMin <tktwin00@gmail.com>
2025-08-08 17:12:02 +0900

    fix: tokens 배열 인덱스 범위 수정

--- waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
+++ waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
@@ -33,10 +33,10 @@ public class CommandProcessor {
 
     private void handleQuit(String[] tokens) {
     	// 식당 ID로 해당 식당 객체 가져오기
-    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[1]);
+    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[2]);
     	
     	// 손님 퇴장 후 입장 대기 중인 다음 손님 번호 목록 반환
-    	ArrayList<String> nextCustomerNum = restaurant.quit(tokens[0], tokens[1]);
+    	ArrayList<String> nextCustomerNum = restaurant.quit(tokens[1], tokens[2]);
     	
     	// 반환된 손님 번호 목록 출력
     	for (int i = 0; i < nextCustomerNum.size(); i++) {
@@ -46,10 +46,10 @@ public class CommandProcessor {
 
     private void handleCancel(String[] tokens) {
     	// 식당 ID로 해당 식당 객체 가져오기
-    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[1]);
+    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[2]);
     	
     	// 예약 취소 여부 반환
-    	boolean isCanceled = restaurant.cancelReservation(tokens[1], tokens[0]);
+    	boolean isCanceled = restaurant.cancelReservation(tokens[2], tokens[1]);
     	
     	// 반환된 예약 취소 여부 출력
     	if (isCanceled) {
@@ -61,10 +61,10 @@ public class CommandProcessor {
 
     private void handleReservation(String[] tokens) {
     	// 식당 객체 가져오기
-    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[2]);
+    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[3]);
     		
     	// 예약 후 대기 번호 반환
-    	int waitingNumber = restaurant.makeReservation(tokens[0], tokens[1]);
+    	int waitingNumber = restaurant.makeReservation(tokens[1], tokens[2]);
     		
     	// 콘솔 출력
     	System.out.println("예약이 완료되었습니다. 당신의 대기 번호는 " + waitingNumber + "번입니다.");
