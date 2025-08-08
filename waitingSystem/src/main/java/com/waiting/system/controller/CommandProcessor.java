784a918dbb46db571db8f455c0c5430043d674d8
TaekkiMin <tktwin00@gmail.com>
2025-08-08 16:26:24 +0900

    feat: 예약 처리 로직 구현

--- waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
+++ waitingSystem/src/main/java/com/waiting/system/controller/CommandProcessor.java
@@ -1,5 +1,7 @@
 package com.waiting.system.controller;
 
+import java.util.ArrayList;
+
 import com.waiting.system.util.UIHandler;
 
 public class CommandProcessor {
@@ -36,6 +38,14 @@ public class CommandProcessor {
     }
 
     private void handleReservation(String[] tokens) {
+    	// 식당 객체 가져오기
+    	Restaurant restaurant = restaurantManager.getRestaurantById(tokens[2]);
+    		
+    	// 예약 후 대기 번호 반환
+    	int waitingNumber = restaurant.makeReservation(tokens[0], tokens[1]);
+    		
+    	// 콘솔 출력
+    	System.out.println("예약이 완료되었습니다. 당신의 대기 번호는 " + waitingNumber + "번입니다.");
     }
 
 }
